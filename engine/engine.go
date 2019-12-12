193190d619679b03ec90e12cb91aa2cf2f11f176
Khilchenko Yehor <khilchenko.yehor@gmail.com>
2019-12-12 19:00:40 +0200

    finish event loop

--- engine/engine.go
+++ engine/engine.go
@@ -1,5 +1,50 @@
 package engine
 
-func main() {
+// Command represents actions that can be performed in a single event loop iteration.
+type Command interface {
+	Execute(handler Handler)
+}
 
+// Handler allows to send commands to an event loop it's associated with. type Handler interface {
+type Handler interface {
+	Post(cmd Command)
+}
+
+// EventLoop struct instance
+type EventLoop struct {
+	messagesQueue []Command
+	infiniteLoop  chan (bool)
+	Await         bool
+}
+
+// Start - begin eventloop
+func (el *EventLoop) Start() {
+	el.infiniteLoop = make(chan bool)
+	go func() {
+		for {
+			if len(el.messagesQueue) == 0 && el.Await {
+				break
+			}
+			if len(el.messagesQueue) != 0 {
+				// shifting one command from queue
+				cmd := el.messagesQueue[0]
+				el.messagesQueue = el.messagesQueue[1:]
+				// executing command
+				cmd.Execute(el)
+			}
+		}
+		el.infiniteLoop <- true
+	}()
+}
+
+// Post - push command to messageQueue
+func (el *EventLoop) Post(cmd Command) {
+	// push command here
+	el.messagesQueue = append(el.messagesQueue, cmd)
+}
+
+// AwaitFinish - wait till eventloop ends its cycle (default not wait)
+func (el *EventLoop) AwaitFinish() {
+	el.Await = true
+	<-el.infiniteLoop
 }
--- main.go
+++ main.go
@@ -1,10 +1,61 @@
 package main
 
 import (
-	"https://github.com/KHYehor/architecture-lab4/engine"
+	"bufio"
+	"crypto/sha1"
+	"fmt"
+	"os"
+	"strings"
+
+	"./engine"
 )
+
+// printCommand - command for priniting messages to console
+type printCommand struct {
+	arg string
+}
+
+// Execute - run command
+func (pc *printCommand) Execute(loop engine.Handler) {
+	fmt.Println(pc.arg)
+}
+
+// hashCommand - command for hashing string with sha1 and print to console
+type hashCommand struct {
+	arg string
+}
+
+// Execute - run command
+func (hc *hashCommand) Execute(loop engine.Handler) {
+	h := sha1.New()
+	h.Write([]byte(hc.arg))
+	bs := h.Sum(nil)
+	loop.Post(&printCommand{arg: string(bs)})
+}
+
+// parse - parsing messages to commands structs
+func parse(commandline string) engine.Command {
+	parts := strings.Fields(commandline)
+	switch parts[0] {
+	case "print":
+		return &printCommand{arg: parts[1]}
+	case "sha1":
+		return &hashCommand{arg: parts[1]}
+	default:
+		return &printCommand{arg: "Syntax Error Unexpected command"}
+	}
+}
 
 func main() {
 	eventLoop := new(engine.EventLoop)
 	eventLoop.Start()
+
+	if input, err := os.Open("./commands.txt"); err == nil {
+		defer input.Close()
+		scanner := bufio.NewScanner(input)
+		for scanner.Scan() {
+			eventLoop.Post(parse(scanner.Text()))
+		}
+	}
+	eventLoop.AwaitFinish()
 }
